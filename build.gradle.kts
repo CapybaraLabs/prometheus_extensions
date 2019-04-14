a0b575c85afd85f28d36d1c01ceb900561edaf9c
Napster <napster@npstr.space>
2019-04-14 16:40:07 +0200

    Fix: Reduce exposed apis to the minimum

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,6 @@ val okHttpVersion = "3.14.1"
 
 dependencies {
     api("io.prometheus:simpleclient:$prometheusVersion")
-    api("io.prometheus:simpleclient_hotspot:$prometheusVersion")
-    api("io.prometheus:simpleclient_logback:$prometheusVersion")
-    api("io.prometheus:simpleclient_common:$prometheusVersion")
-    api("io.prometheus:simpleclient_caffeine:$prometheusVersion")
 
     compileOnly("net.ttddyy:datasource-proxy:$dsProxyVersion")
     compileOnly("com.squareup.okhttp3:okhttp:$okHttpVersion")
