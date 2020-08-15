dc8af0cfa33f58871caf2026238e587fb5a9434e
Napster <napster@npstr.space>
2020-08-15 20:32:45 +0200

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,18 +7,23 @@ plugins {
 group = "space.npstr.prometheus_extensions"
 version = "0.0.1-SNAPSHOT"
 
+java {
+    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
 repositories {
     jcenter()
 }
 
-val prometheusVersion   = "0.8.0"
-val dsProxyVersion      = "1.5.1"
-val jdaVersion          = "4.1.0_100"
-val troveVersion        = "3.0.3"
-val fastutilVersion     = "8.3.0"
-val jUnitVersion        = "5.5.2"
-val mockitoVersion      = "3.2.4"
-val assertJVersion      = "3.14.0"
+val prometheusVersion = "0.9.0"
+val dsProxyVersion = "1.7"
+val jdaVersion = "4.2.0_191"
+val troveVersion = "3.0.3"
+val fastutilVersion = "8.4.1"
+val jUnitVersion = "5.6.2"
+val mockitoVersion = "3.4.6"
+val assertJVersion = "3.16.1"
 
 dependencies {
     api("io.prometheus:simpleclient:$prometheusVersion")
@@ -30,7 +35,7 @@ dependencies {
     compileOnly("net.sf.trove4j:trove4j:$troveVersion")
     compileOnly("it.unimi.dsi:fastutil:$fastutilVersion")
 
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
     testImplementation("net.dv8tion:JDA:$jdaVersion")
     testImplementation("org.mockito:mockito-core:$mockitoVersion")
