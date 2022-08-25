e5806bfa1557c728da230163b627887652c14c29
Napster <napster@npstr.space>
2022-08-25 23:42:20 +0200

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,9 +31,9 @@ val jdaVersion = "4.4.0_352"
 val troveVersion = "3.0.3"
 val fastutilVersion = "8.5.8"
 // see https://oss.sonatype.org/content/repositories/snapshots/com/discord4j/discord4j-core/
-val d4jCoreVersion = "3.2.2"
-val jUnitVersion = "5.8.2"
-val mockitoVersion = "4.6.1"
+val d4jCoreVersion = "3.2.3"
+val jUnitVersion = "5.9.0"
+val mockitoVersion = "4.7.0"
 val assertJVersion = "3.23.1"
 
 dependencies {
